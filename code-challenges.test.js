a65939d683ec17a7396cc3f5b77d58c0fc2cf6ea
ninerboy81 <luis.ramos07@yahoo.com>
2022-06-25 09:56:50 -0700

    pushing to save

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,43 +15,121 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
+//const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+//const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
 
 // b) Create the function that makes the test pass.
-
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+//pseudo code:
+// declare a function called evenDiv that takes in a number
+// set a parameter called odd
+// use if statement to check if odd is divisible by 3 using modulo % 3 === 0
+// if it is, return the string "is divisible by three"
+// if it is not, return the string "is not divisible by three"
 
+describe("evenDiv", () => {
+    it("should return true if number is evenly divisble by three", () => {
+        const num1 = 15
+        // Expected output: "15 is divisible by three"
+        const num2 = 0
+        // Expected output: "0 is divisible by three"
+        const num3 = -7
+        // Expected output: "-7 is not divisible by three"
+        expect(evenDiv(num1)).toEqual("15 is divisible by three")
+        expect(evenDiv(num2)).toEqual("0 is divisible by three")
+        expect(evenDiv(num3)).toEqual("-7 is not divisible by three")
+    })
+})
 
+// ReferenceError: evenlyDiv is not defined
+  
 // b) Create the function that makes the test pass.
 
-
+const evenDiv = (odd) => {
+    if (odd % 3 === 0) {
+        return odd + " is divisible by three"
+    } else {
+        return odd + " is not divisible by three"
+    }
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+//pseudo code:
+// declare a function called capitalize 
+// set a parameter called array
+// use .map to iterate through the array
+// use .charAt to get the first letter of the word using (0) since itll be an array the first letter is index 0
+// use .toUpperCase to make the first letter uppercase
+// use .slice to bring back the rest of the string starting at 1 since (0) is the first letter then (1) is the second letter
+
+
+describe("capitalize", () => {
+    it("should return an array with all the words capitalized",() => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+    expect(capitalize(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+    expect(capitalize(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+} ) 
+})
+
+// ReferenceError: capitalize is not defined
 
 
 // b) Create the function that makes the test pass.
+
+const capitalize = (array) => {
+    return array.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
@@ -58,12 +137,39 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deducti
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+// pseudo code:
+// declare a function called vowelIndex that takes in string
+// declare a variable called vowels that has a string called "aeiou" for the vowels
+// set a parameter called string
+// use for loop to iterate through the string to look for the vowels
+// if the vowels includes the string[i] then return the index
+// return 
 
+describe ("vowelIndex", () => {
+    it("should return the index of the first vowel", () => {
+    const vowelTester1 = "learn"
+    // Expected output: 1
+    const vowelTester2 = "academy"
+    // Expected output: 0
+    const vowelTester3 = "challenges"
+    // Expected output: 2
+    expect(vowelIndex(vowelTester1)).toEqual(1)
+    expect(vowelIndex(vowelTester2)).toEqual(0)
+    expect(vowelIndex(vowelTester3)).toEqual(2)
+    }
+    )
+}
+)   
+
+// ReferenceError: vowelIndex is not defined
 
 // b) Create the function that makes the test pass.
+
+const vowelIndex = (string) => {
+const vowels = "aeiou"
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i])) {
+            return i
+        }
+    }
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: .split() should return a string that has placed a space between each letter of the string
+// b) Verify and explain: Using .split returned an array of strings containing the letters of the string. All the letters were separated into their own strings.
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ var cohort = "Delta 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will return Hello, LEARN Student! because using string interpolation along with ${} the string "LEARN Student" was inserted into the string "Hello, "
+// b) Verify and explain: What i got back was undefined. The function was declared with a parameter, but (name) was never assigned a value.
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Using .map() should return an array of numbers that have been multiplied by 2 giving the answer of [8, 10, 12, 14, 16]
+// b) Verify and explain: Since the array was created with the numbers 4, 5, 6, 7, 8, the numbers were multiplied by 2 and the result was placed in the array.
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Starting with an array and using .filter() as well as modulo 2 !== 0 should return an array of numbers that have been filtered to only include odd numbers.
+// b) Verify and explain: An array with numbers using .filter() with syntax value of modulo 2 !== 0 means that if it is divisible by 2 then it will be not show up in the array.
 
 
 // --------------------5) What will this log?
@@ -51,10 +51,10 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Calling myCodingSkills.languages[0] should return JavaScript since it is the first element in the array.
+// b) Verify and explain: Using .languages[0] means that you are calling the first indexed element in the array because of [0].
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +67,7 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will return "George" , "Bravo", and 2022 since the constructor has nested student, cohort, and year and we used "George" with "new" in the variable learnStudent.
+// b) Verify and explain: So i missed that each "this."" included an "=" meaning that the return would have student:, cohort:, and year:.
